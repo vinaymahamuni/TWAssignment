ec4ab5904b49719205634dabe26456201f751e8e
vinayam <vinayam@in-blr-vinayam.local>
2017-07-16 12:46:13 +0530

    add functionality to draw diamond with name

--- src/Diamond.java
+++ src/Diamond.java
@@ -34,12 +34,26 @@ public class Diamond {
         System.out.print(output);
     }
 
+    static void drawDiamondWithName(int N, String name){
+
+        for (int counter = 1; counter < N; counter++) {
+            drawHorizontalLineOfGivenCharacter(N - counter, ' ');
+            drawHorizontalLineOfGivenCharacter(2 * counter - 1, '*');
+            System.out.println();
+        }
+
+        System.out.println(name);
+        drawReverseIsoscelesTriangle(N);
+    }
     public static void main(String args[]) {
         System.out.println("IsoscelesTriangle");
         drawIsoscelesTriangle(5);
 
-        System.out.println("Diamond");
+        System.out.println("\n\nDiamond");
         drawDiamond(5);
+
+        System.out.println("\n\ndiamond with name");
+        drawDiamondWithName(3,"vinay");
 
     }
 }
