46397bb242ea59be285cfe6ed1ef7c2909d8d22b
vinayam <vinayam@in-blr-vinayam.local>
2017-07-16 11:34:02 +0530

    Created Triangle class. added functionaliiy to print asterisk

---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+/**
+ * Created by vinayam on 7/16/17.
+ */
+public class Triangle {
+
+    static void drawOneAsterisk(){
+        System.out.print("*");
+    }
+
+    public static void main(String[] args){
+            drawOneAsterisk();
+    }
+}
